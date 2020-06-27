1e4ef8614c5358560567ce57d2a6865860c91f3d
Alyreza <fanaticboy666@gmail.com>
2020-06-27 12:55:31 +0430

    customize list of currency tweak return string to valid form

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-'use strict'
-const arrify           = require('arrify');
-const pAny             = require('p-any');
-const pify             = require('pify');
-const pTimeout         = require('p-timeout');
-const bitcoinRegex     = require('bitcoin-regex');
-const ethereumRegex    = require('ethereum-regex');
-const litecoinRegex    = require('litecoin-regex');
-const dogecoinRegex    = require('dogecoin-regex');
-const bitcoincashRegex = require('bitcoincash-regex');
-const dashRegex        = require('dash-regex');
-const moneroRegex      = require('monero-regex');
-const rippleRegex      = require('ripple-regex');
-const neoRegex         = require('neo-regex');
+"use strict";
+const arrify = require("arrify");
+const pAny = require("p-any");
+const pTimeout = require("p-timeout");
+const bitcoinRegex = require("bitcoin-regex");
+const ethereumRegex = require("ethereum-regex");
+const litecoinRegex = require("litecoin-regex");
+const dogecoinRegex = require("dogecoin-regex");
+const bitcoincashRegex = require("bitcoincash-regex");
+const dashRegex = require("dash-regex");
 
 function detectCrypto(address) {
-	if (bitcoinRegex({exact: true}).test(address)) return 'BTC/BCH';
-	else if (ethereumRegex({exact: true}).test(address)) return 'ETH';
-	else if (litecoinRegex({exact: true}).test(address)) return 'LTC';
-	else if (dogecoinRegex({exact: true}).test(address)) return 'DOGE';
-	else if (dashRegex({exact: true}).test(address)) return 'DASH';
-	else if (moneroRegex({exact: true}).test(address)) return 'XMR';
-	else if (rippleRegex({exact: true}).test(address)) return 'XRP';
-	else if (bitcoincashRegex.format('cashaddr', {exact: true}).test(address)) return 'BCH';
-	else if (neoRegex({exact: true}).test(address)) return 'NEO';
-	else return 'Cryptocurrency could not be detected'
+  if (bitcoinRegex({ exact: true }).test(address)) return "Bitcoin";
+  else if (ethereumRegex({ exact: true }).test(address)) return "Ethereum";
+  else if (litecoinRegex({ exact: true }).test(address)) return "LiteCoin";
+  else if (dogecoinRegex({ exact: true }).test(address)) return "DogeCoin";
+  else if (dashRegex({ exact: true }).test(address)) return "Dash";
+  else if (bitcoincashRegex.format("cashaddr", { exact: true }).test(address))
+    return "BitcoinCash";
+  else return "Cryptocurrency could not be detected";
 }
 
 module.exports = (dests, opts) => {
   opts = opts || {};
-  opts.timeout = typeof opts.timeout === 'number' ? opts.timeout : 5000;
+  opts.timeout = typeof opts.timeout === "number" ? opts.timeout : 5000;
 
   const p = pAny(arrify(dests).map(detectCrypto));
-  return pTimeout(p, opts.timeout).catch(() => 'Cryptocurrency could not be detected');
+  return pTimeout(p, opts.timeout).catch(
+    () => "Cryptocurrency could not be detected"
+  );
 };
